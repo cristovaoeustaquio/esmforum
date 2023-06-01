a0d8d8e089517e363fb25d08e565a4630303a9a3
cristovaoeustaquio <67655102+cristovaoeustaquio@users.noreply.github.com>
2023-05-31 21:46:49 -0300

    Exercicio 2: teste de integração

--- testes/modelo.test.js
+++ testes/modelo.test.js
@@ -21,5 +21,16 @@ test('Testando cadastro de três perguntas', () => {
   expect(perguntas[0].texto).toBe('1 + 1 = ?');
   expect(perguntas[1].texto).toBe('2 + 2 = ?');
   expect(perguntas[2].num_respostas).toBe(0);
+  expect(modelo.get_pergunta(2)).toBe('3 + 3 = ?')
   expect(perguntas[1].id_pergunta).toBe(perguntas[2].id_pergunta-1);
+});
+
+test('Testando cadastro de três respostas', () => {
+  modelo.cadastrar_resposta(0, '2');
+  modelo.cadastrar_resposta(1, '4');
+  modelo.cadastrar_resposta(1, '3');
+  expect(modelo.get_respostas(0)).toBe([{"id_pergunta": 0, "id_resposta": 27, "texto": "2"}]);
+  expect(modelo.get_num_respostas(0)).toBe(1);
+  expect(modelo.get_num_respostas(1)).toBe(2);
+  expect(modelo.get_num_respostas(2)).toBe(0);
 });
